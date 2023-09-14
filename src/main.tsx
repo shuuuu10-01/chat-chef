b224e913b8384ea4843c5adf46704758686eb349
shuuuu10-01 <koron.konan123@gmail.com>
2023-09-14 20:15:49 +0900

    delete: tailwindの削除

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CustomRouter from 'src/router.tsx';
 
-import './styles/globals.scss';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider resetCSS>
