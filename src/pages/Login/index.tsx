4708576300e765bb716d892dbbe0d48b9b02d882
shuuuu10-01 <koron.konan123@gmail.com>
2023-09-15 12:47:36 +0900

    feat: ログイン成功後、トップページに遷移するように実装

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,6 +9,9 @@ import {
 } from '@chakra-ui/react';
 import { FC } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
+import LogoIcon from 'src/components/LogoIcon';
 
 import { LoginFormData } from 'src/types/auth';
 
@@ -17,6 +20,7 @@ import useLogin from './useLogin';
 import styles from './styles.module.scss';
 
 const Login: FC = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -26,18 +30,17 @@ const Login: FC = () => {
   const { login, isLoading } = useLogin();
 
   const submit: SubmitHandler<LoginFormData> = (data) => {
-    login(data);
+    login(data).then(() => navigate('/'));
   };
 
   return (
-    <Flex
-      height={'100dvh'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      flexDirection={'column'}>
-      <Heading mb={8}>Chat Chef</Heading>
+    <Flex height="100dvh" alignItems="center" justifyContent="center" flexDirection="column">
+      <Heading mb={8} display="flex" alignItems="center" gap="10px">
+        <LogoIcon />
+        Chat Chef
+      </Heading>
       <form className={styles.form} onSubmit={handleSubmit(submit)}>
-        <FormControl mb={4} isInvalid={!!errors.email} textAlign={'start'}>
+        <FormControl mb={4} isInvalid={!!errors.email} textAlign="start">
           <FormLabel mb={1} htmlFor="email">
             メールアドレス
           </FormLabel>
