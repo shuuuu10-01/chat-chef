268853aeb5d3a4541566395c05a2842f0b3db4fd
shuuuu10-01 <koron.konan123@gmail.com>
2023-09-28 00:09:47 +0900

    fix: corsエラーの除去

--- firebaseSDK/functions/src/index.ts
+++ firebaseSDK/functions/src/index.ts
@@ -3,9 +3,16 @@ import { verifyIdToken } from './utils/auth';
 import { gptTalk } from './utils/chat-gpt';
 import { postFirestore } from './utils/firestore';
 import './plugins/firebase';
+import { extractTokenFromBearer } from './utils/string';
 
 export const chat = onRequest(async (request, response) => {
-  const idToken = request.headers.authorization;
+  response.set('Access-Control-Allow-Origin', process.env.WEB_APP_PATH);
+  response.set('Access-Control-Allow-Headers', '*');
+  if (request.method === 'OPTIONS') {
+    response.status(204).end();
+  }
+
+  const idToken = extractTokenFromBearer(request.headers.authorization);
 
   if (!idToken) {
     response.status(403).json({ error: 'Unauthorized' });
@@ -20,7 +27,7 @@ export const chat = onRequest(async (request, response) => {
     return;
   }
 
-  const ingredients = request.body.ingredients;
+  const ingredients = request.body.data.ingredients;
   const result = await gptTalk(ingredients);
 
   if (!result.content) {
