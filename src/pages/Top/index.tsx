79e368b1225cb8e9beedd10abba7d5c03bad507d
shuuuu10-01 <koron.konan123@gmail.com>
2023-09-28 23:22:47 +0900

    feat: cloud functionsの関数を実装

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,5 +1,7 @@
 import { Box, Heading } from '@chakra-ui/react';
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { actions, useAppDispatch, useAppSelector } from 'src/store';
 
 import Header from 'src/components/Header';
 import SelectAccordion from 'src/components/SelectAccordion';
@@ -10,13 +12,43 @@ import SelectProvider from 'src/pages/Top/SelectProvider';
 
 import { runChatGPT } from 'src/repositories/functions';
 
-import { SelectType } from 'src/types/category';
+import { CATEGORY, SelectType } from 'src/types/category';
+
+import { fetchFireStore } from 'src/utils/firestore';
 
 const Top: FC = () => {
-  const handleSubmit = useCallback(async (data: SelectType[]) => {
-    const ingredients = data.map((d) => `${d.categoryName}の${d.label}`);
-    await runChatGPT(ingredients);
-  }, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { isAdmin } = useAppSelector((state) => state.user);
+
+  const handleSubmit = useCallback(
+    async (data: SelectType[]) => {
+      if (!isAdmin) {
+        alert('権限がないため実行できません');
+        return;
+      }
+
+      setIsLoading(true);
+      const ingredients = formatIngredients(data);
+      runChatGPT(ingredients)
+        .then(() => {
+          dispatch(actions.suggestion.toggleRefreshFlag());
+          navigate('/suggest');
+        })
+        .catch((e) => {
+          console.log(e);
+          alert('エラーが発生しました。もう一度お試しください。');
+        })
+        .finally(() => setIsLoading(false));
+    },
+    [isAdmin, dispatch, navigate],
+  );
+
+  useEffect(() => {
+    fetchFireStore();
+  });
+
   return (
     <Box>
       <Header />
@@ -27,7 +59,7 @@ const Top: FC = () => {
         <Box>
           <SelectAccordion />
           <SelectedItem />
-          <SelectSubmit onSubmit={handleSubmit} />
+          <SelectSubmit isLoading={isLoading} onSubmit={handleSubmit} />
         </Box>
       </SelectProvider>
     </Box>
@@ -35,3 +67,12 @@ const Top: FC = () => {
 };
 
 export default Top;
+
+const formatIngredients = (data: SelectType[]) => {
+  return data.map((d) => {
+    if (d.categoryId === CATEGORY.FISH) {
+      return d.label;
+    }
+    return `${d.categoryName}の${d.label}`;
+  });
+};
