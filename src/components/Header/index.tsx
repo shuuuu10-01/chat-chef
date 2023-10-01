1d519d103d4a77eb62ec3010023b2c0fc82d87a2
shuuuu10-01 <koron.konan123@gmail.com>
2023-10-01 10:36:13 +0900

    feat: Headerのリンクの幅を調整

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Link as ChakraLink } from '@chakra-ui/react';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Z_INDEX_HEADER } from 'src/constants/z-index';
@@ -16,12 +16,19 @@ const Header: FC = () => {
       borderColor='gray.300'
       backgroundColor='white'
       zIndex={Z_INDEX_HEADER}>
-      <Link to='/'>
-        <Heading as='h3' size='xl' display='flex' alignItems='center' gap='5px'>
+      <Heading as='h3' size='xl'>
+        <ChakraLink
+          as={Link}
+          to='/'
+          display='flex'
+          alignItems='center'
+          gap='5px'
+          _hover={{ textDecoration: 'none' }}
+          width='fit-content'>
           <LogoIcon />
           Chat Chef
-        </Heading>
-      </Link>
+        </ChakraLink>
+      </Heading>
     </Box>
   );
 };
