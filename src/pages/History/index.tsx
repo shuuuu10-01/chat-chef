f971f1b1135878fcadb88fe8ab86a5ea8c63c33f
shuuuu10-01 <koron.konan123@gmail.com>
2023-10-27 18:45:00 +0900

    feat: 履歴ページの土台を実装

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+
+import Header from 'src/components/Header';
+
+const History: FC = () => {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+};
+
+export default History;
