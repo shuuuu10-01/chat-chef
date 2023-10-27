55789bb9a32b918d128107d0f337423ee3fe0ffc
shuuuu10-01 <koron.konan123@gmail.com>
2023-10-27 18:42:30 +0900

    change: コンポーネントを使うように改修

--- src/pages/Suggest/index.tsx
+++ src/pages/Suggest/index.tsx
@@ -1,78 +1,51 @@
-import { Box, Button, Flex, Text, Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Text, Link as ChakraLink, Flex } from '@chakra-ui/react';
 import { FC, useEffect, useMemo } from 'react';
-import Markdown from 'react-markdown';
 import { Link, useSearchParams } from 'react-router-dom';
 import { useAppSelector } from 'src/store';
-import './github-markdown.css';
 
+import Chat from 'src/components/Chat';
 import Header from 'src/components/Header';
-import LogoIcon from 'src/components/LogoIcon';
-
-import styles from './styles.module.scss';
+import Pagination from 'src/components/Pagination';
 
 const Suggest: FC = () => {
   const { contents, refreshFlag } = useAppSelector((state) => state.suggestion);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const index = Number(searchParams.get('index') || 0);
+  const page = Number(searchParams.get('page') || 1);
+  const currentContent = useMemo(() => contents[page - 1], [contents, page]);
 
   const toPrevious = () => {
-    setSearchParams({ index: String(index - 1) });
+    setSearchParams({ index: String(page - 1) });
   };
   const toNext = () => {
-    setSearchParams({ index: String(index + 1) });
+    setSearchParams({ index: String(page + 1) });
   };
 
-  const disabledPrevious = useMemo(() => {
-    return index < 1;
-  }, [index]);
-  const disabledNext = useMemo(() => {
-    return contents.length - 1 <= index;
-  }, [contents.length, index]);
-
   useEffect(() => {
-    if (contents.length === 0 || index < 0 || !!contents[index]) return;
+    if (contents.length === 0 || page < 1 || !!currentContent) return;
     // クエリに不適切な値が設定された場合、クエリをリセット
     setSearchParams();
-  }, [contents, index, setSearchParams]);
+  }, [currentContent, contents, page, setSearchParams]);
 
   return (
     <Box>
       <Header />
       {contents.length > 1 && (
-        <Flex justifyContent='space-between' pt='4' px='2'>
-          <Button onClick={toPrevious} variant='outline' isDisabled={disabledPrevious}>
-            ← 前へ
-          </Button>
-          <Button onClick={toNext} variant='outline' isDisabled={disabledNext}>
-            次へ →
-          </Button>
-        </Flex>
+        <Pagination
+          toPrevious={toPrevious}
+          toNext={toNext}
+          page={page}
+          lastPage={contents.length}
+        />
       )}
-      {!!contents[index] && (
-        <Box width='90%' my='2' mx='auto'>
-          <Flex alignItems='start' justifyContent='flex-end' pl='62px' mb='4'>
-            <p className={styles['user-message']}>
-              {convertUserMessage(contents[index].ingredients)}
-            </p>
-          </Flex>
-          <div className={styles.chef}>
-            <LogoIcon className={styles.icon} size='40px' />
-            <Markdown className={`markdown-body ${styles.suggest}`}>
-              {contents[index]?.chatGPT}
-            </Markdown>
-          </div>
-        </Box>
-      )}
+      {!!currentContent && <Chat content={currentContent} />}
       {contents.length > 1 && (
-        <Flex justifyContent='space-between' px='2' py='4'>
-          <Button onClick={toPrevious} variant='outline' isDisabled={disabledPrevious}>
-            ← 前へ
-          </Button>
-          <Button onClick={toNext} variant='outline' isDisabled={disabledNext}>
-            次へ →
-          </Button>
-        </Flex>
+        <Pagination
+          toPrevious={toPrevious}
+          toNext={toNext}
+          page={page}
+          lastPage={contents.length}
+        />
       )}
       {contents.length === 0 && !refreshFlag && (
         <Flex flexDirection='column' alignItems='center' justifyContent='center' mt='20' gap='2'>
@@ -88,7 +61,3 @@ const Suggest: FC = () => {
 };
 
 export default Suggest;
-
-const convertUserMessage = (ingredients: string[]) => {
-  return `${ingredients.join(' と ')} を使った料理を教えて`;
-};
