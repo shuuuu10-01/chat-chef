71ebc80eb2fcff8744bba940e108c0401e344714
shuuuu10-01 <koron.konan123@gmail.com>
2024-05-12 23:15:36 +0900

    fix: includeAssetsの内容を修正

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['cook.svg'],
       injectRegister: 'auto',
       manifest: {
         name: 'Chat Chef',
