fd4451571e8da4255a6b577331031081319cb939
shuuuu10-01 <koron.konan123@gmail.com>
2024-05-12 22:56:03 +0900

    add: vite-plugin-pwaの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,49 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Chat Chef',
+        short_name: 'chat-chef',
+        description: '食材を選択し、その食材を用いた料理を提案してくれるアプリ',
+        theme_color: '#F6AD55',
+        background_color: '#FFFFFF',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: 'src', replacement: '/src' }],
   },
