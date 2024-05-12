224cba1ff3f3ef7a43649c9476fb66ced51ef0da
shuuuu10-01 <koron.konan123@gmail.com>
2024-05-12 23:28:42 +0900

    fix: pwa設定の画像パスの修正

--- vite.config.ts
+++ vite.config.ts
@@ -19,28 +19,23 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-180x180.png',
-            sizes: '180x180',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
+            src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
